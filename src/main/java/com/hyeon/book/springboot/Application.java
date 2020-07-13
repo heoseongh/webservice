e0a71bdbf612465639a78e12759ce306ca4117ba
heoseongh <heoseongh>
2020-07-14 01:21:18 +0900

    lombok_기능_테스트

---
+++ src/main/java/com/hyeon/book/springboot/Application.java
@@ -0,0 +1,11 @@
+package com.hyeon.book.springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//앞으로 만들 프로젝트의 메인 클래스
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
